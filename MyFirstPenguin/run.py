c0e84664638b3f77e3ee81b037dd0211b90ec6a3
Tania Holst <holst.tania@gmail.com>
2018-11-06 20:20:55 +0100

    syn

--- MyFirstPenguin/run.py
+++ MyFirstPenguin/run.py
@@ -80,7 +80,7 @@ def findMagnitude(x1, y1, x2, y2):
 
 def chooseAction(body):
     action = moveTowardsCenterOfMap(body)
-    if len(bonusTiles) >= 1 && bonusTiles[0].type == "strength":
+    if len(bonusTiles) >= 1 & bonusTiles[0].type == "strength":
         action = goToHeart(body)
     return action
 
